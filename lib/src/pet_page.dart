7f6a6df36f1482537a99a2436766b79f638ec683
godelivadiva <godeliva.divayu@students.utdi.ac.id>
2023-06-25 19:53:10 +0700

    [TEST] trial menggunakan data album dari contoh

--- lib/src/pet_page.dart
+++ lib/src/pet_page.dart
@@ -1,44 +1,69 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
-/* Class Pet befungsi untuk mendefinisikan property dari masing-masing pet yang
-didalamnya menampung name, gender, age, dan imageUrl */
-class Pet {
-  final String name;
-  final String gender;
-  final double age;
-  final String imageUrl;
-
-  Pet({required this.name, required this.gender, required this.age, required this.imageUrl});
+Future<Pets> fetchPets() async {
+  final response = await http
+      // .get(Uri.parse('https://63afb929649c73f572c113ad.mockapi.io/api/v1/cat_adoption_list'));
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  https://jsonplaceholder.typicode.com/albums/1
+  if (response.statusCode == 200) {
+    return Pets.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Gagal menampilkan data!');
+  }
 }
 
-/* PetListScreen widget menampilkan list dari pets dengan menggunakan
-ListView.builder wdiget. */
-class PetListScreen extends StatelessWidget {
-  // fungsi untuk melakukan permintaan HTTP ke REST API dan mendapatkan responsenya:
-  Future<List<dynamic>> fetchData() async {
-    final response = await http.get(Uri.parse('https://63afb929649c73f572c113ad.mockapi.io/api/v1/cat_adoption_list'));
-    if (response.statusCode == 200) {
-      // Konversi response JSON menjadi List<dynamic>
-      List<dynamic> data = json.decode(response.body);
-      return data;
-    } else {
-      throw Exception('Gagal menampilkan data!');
-    }
+class Pets {
+  final int userId;
+  final int id;
+  final String title;
+  // final String name;
+  // final String gender;
+  // final double age;
+  // final String imageUrl;
+
+  const Pets({
+    required this.userId,
+    required this.id,
+    required this.title,
+    // required this.name,
+    // required this.gender,
+    // required this.age,
+    // required this.imageUrl,
+  });
+
+  factory Pets.fromJson(Map<String, dynamic> json) {
+    return Pets(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      // name: json['name'],
+      // gender: json['gender'],
+      // age: json['age'],
+      // imageUrl: json['imageUrl'],
+
+    );
   }
-  /* Define sebuah array untuk menampung nama, gender, umur, dan link dari
-  masing-masing pets yang akan ditambilkan ke dalam variabel pets. Untuk
-  menyimpan source dari gambar, menggunakan variabel imageUrl */
-  final List<Pet> pets = [
-    Pet(name: 'Acle', gender: 'Jantan', age: 4, imageUrl: 'assets/images/acle.jpg'),
-    Pet(name: 'Joko', gender: 'Jantan', age: 1.5, imageUrl: 'assets/images/joko.jpg'),
-    Pet(name: 'Gato', gender: 'Betina', age: 3, imageUrl: 'assets/images/gato.jpg'),
-    Pet(name: 'Mao', gender: 'Betina', age: 3, imageUrl: 'assets/images/mao.jpg'),
-    Pet(name: 'Neko', gender: 'Jantan', age: 3, imageUrl: 'assets/images/neko.jpg'),
-    Pet(name: 'Pytho', gender: 'Betina', age: 4, imageUrl: 'assets/images/pytho.jpg'),
-  ];
+}
+
+class PetListScreen extends StatefulWidget {
+  const PetListScreen ({super.key});
+
+  @override
+  State<PetListScreen> createState() => _PetListScreenState();
+}
+
+class _PetListScreenState extends State<PetListScreen> {
+  late Future<Pets> futurePets;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePets = fetchPets();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,36 +71,36 @@ class PetListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pet List'),
       ),
-      body: ListView.builder(
-        itemCount: pets.length,
-        itemBuilder: (context, index) {
-          /* Card dan ListTile widgets digunakan untuk membuat kartu for each pet
-          di dalam array.*/
-          return Card(
-            child: ListTile(
-              /* CircleAvatar class untuk menampilkan image ke dalam Card
-              dengan backgroundImage property dengan value AssetImage karena
-              akan mengambil dari folder assets */
-              leading: CircleAvatar(
-                backgroundImage: AssetImage(pets[index].imageUrl),
-              ),
-              /* Text widget untuk menampilkan nama, gender, dan umur di dalam
-              card */
-              title: Text(pets[index].name),
-              subtitle: Text('${pets[index].gender} - ${pets[index].age} tahun'),
-              onTap: () {
-                String pet_name = pets[index].name;
-                // menampilkan text dengan snackbar saat klik each ListTile
-                ScaffoldMessenger.of(context).showSnackBar( // method for display SnackBar
-                  SnackBar(
-                    content: Text('Kamu memilih $pet_name !'), // text yang ditampilkan
-                    duration: Duration(milliseconds: 500), // durasi menampilkan text
+      body: Center(
+        child: FutureBuilder<Pets>(
+          future: futurePets,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      // backgroundImage: AssetImage(pet['imageUrl']),
+                    ),
+                    title: Text(snapshot.data!.title),
+                    subtitle: Text('${snapshot.data!.userId} tahun'),
+                    onTap: () {
+                      String petName = snapshot.data!.title;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Kamu memilih $petName !'),
+                          duration: Duration(milliseconds: 500),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-          );
-        },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
